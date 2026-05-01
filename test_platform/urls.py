f3337e23d6f29d8138cb27af9b43b94db84fa1fb
Vladislav Takhtau <takhtau@gmail.com>
2026-05-01 04:23:21 +0300

    🔧 Возвращаем next_page для логаута

--- test_platform/urls.py
+++ test_platform/urls.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', LogoutView.as_view(next_page='/tests/login/'), name='logout'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', lambda request: redirect('/tests/login/')),
