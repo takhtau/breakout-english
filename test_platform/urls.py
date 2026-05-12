f5220dc3839113cadcd9ca2c4d25f15c894856ec
Vladislav Takhtau <takhtau@gmail.com>
2026-05-12 12:53:30 +0300

    🔧 Возвращён маршрут register/invite

--- test_platform/urls.py
+++ test_platform/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.contrib.auth.views import LogoutView
+from tests import views as tests_views
 
 urlpatterns = [
     path('accounts/logout/', LogoutView.as_view(next_page='/tests/login/'), name='logout'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', lambda request: redirect('/tests/login/')),
     path('tests/', include('tests.urls')),
+    path('register/invite/<uuid:code>/', tests_views.register_by_invite, name='register_by_invite'),
 ]
